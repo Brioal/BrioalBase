de081e04471b57afd462eea30ef608065274b56a
Brioal <brioal@foxmai.com>
2017-08-28 18:50:27 +0800

    添加CacheUtil删除功能和过时作废功能

--- baselib/src/main/java/com/brioal/baselib/utils/CacheUtil.java
+++ baselib/src/main/java/com/brioal/baselib/utils/CacheUtil.java
@@ -6,6 +6,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.io.Serializable;
 
 /**
  * email:brioal@foxmail.com
@@ -14,7 +15,67 @@ import java.io.FileWriter;
  */
 
 public class CacheUtil<T> {
+    class CacheBean implements Serializable {
+        T mResult;
+        long mTime;
 
+        public T getResult() {
+            return mResult;
+        }
+
+        public void setResult(T result) {
+            mResult = result;
+        }
+
+        public long getTime() {
+            return mTime;
+        }
+
+        public void setTime(long time) {
+            mTime = time;
+        }
+    }
+
+    /**
+     *
+     * @param context
+     * @param name
+     * @param durationTime 间隔时间
+     * @return
+     */
+    public T getCache(Context context, String name,long durationTime) {
+        T result = null;
+        try {
+            File targetFile = new File(context.getFilesDir().getAbsolutePath() + "/" + name);
+            if (!targetFile.exists()) {
+                return null;
+            }
+            String cacheStr = IOUtil.readStr(new FileInputStream(targetFile));
+            CacheBean bean = (CacheBean) SerializeUtil.deSerialize(cacheStr);
+            if (bean == null) {
+                result = null;
+            } else {
+                long saveTime = bean.getTime();
+                if (System.currentTimeMillis() - saveTime < durationTime) {
+                    result = bean.getResult();
+                } else {
+                    return null;
+                }
+            }
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 获取缓存
+     *
+     * @param context
+     * @param name
+     * @return
+     */
     public T getCache(Context context, String name) {
         T result = null;
         try {
@@ -23,7 +84,12 @@ public class CacheUtil<T> {
                 return null;
             }
             String cacheStr = IOUtil.readStr(new FileInputStream(targetFile));
-            result = (T) SerializeUtil.deSerialize(cacheStr);
+            CacheBean bean = (CacheBean) SerializeUtil.deSerialize(cacheStr);
+            if (bean == null) {
+                result = null;
+            } else {
+                result = bean.getResult();
+            }
             return result;
         } catch (Exception e) {
             e.printStackTrace();
@@ -31,13 +97,45 @@ public class CacheUtil<T> {
         }
     }
 
+    /**
+     * 删除备份
+     *
+     * @param context
+     * @param name
+     * @return
+     */
+    public boolean deleteCache(Context context, String name) {
+        try {
+            File targetFile = new File(context.getFilesDir().getAbsolutePath() + "/" + name);
+            if (targetFile.exists()) {
+                targetFile.delete();
+                return true;
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * 保存缓存
+     *
+     * @param context
+     * @param result
+     * @param name
+     * @return
+     */
     public boolean saveCache(Context context, T result, String name) {
         try {
             File targetFile = new File(context.getFilesDir().getAbsolutePath() + "/" + name);
             if (targetFile.exists()) {
                 targetFile.delete();
             }
-            String cacheStr = SerializeUtil.serialize(result);
+            CacheBean bean = new CacheBean();
+            bean.setResult(result);
+            bean.setTime(System.currentTimeMillis());
+            String cacheStr = SerializeUtil.serialize(bean);
             BufferedWriter bw = new BufferedWriter(new FileWriter(targetFile));
             bw.write(cacheStr);
             bw.flush();
